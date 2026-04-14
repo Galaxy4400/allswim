cf1d70355e713c4bee16e8d1ba69d39cb403a6ae
Moiseev Evgeny <moiseevEO@yandex.ru>
2026-04-14 10:53:25 +0300

    commit

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -3,6 +3,7 @@ import notify from 'gulp-notify';
 import newer from 'gulp-newer';
 import ifPlugin from 'gulp-if';
 import browserSync from 'browser-sync';
+import fileInclude from 'gulp-file-include';
 
 export const plugins = {
   plumber,
@@ -10,4 +11,5 @@ export const plugins = {
   newer,
   if: ifPlugin,
   browsersync: browserSync.create(),
+  fileInclude,
 };
--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,11 @@
 export const html = () => {
   return app.gulp
     .src(app.path.src.html, { allowEmpty: true })
-    .pipe(app.plugins.newer(app.path.build.html))
+    .pipe(
+      app.plugins.fileInclude({
+        prefix: '@@',
+        basepath: '@file',
+      }),
+    )
     .pipe(app.gulp.dest(app.path.build.html));
 };
