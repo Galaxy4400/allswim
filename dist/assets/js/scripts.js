a4be6dd0077fda25620ae6eb3d6c59650ca4626e
Moiseev Evgeny <moiseevEO@yandex.ru>
2026-04-15 11:29:34 +0300

    commit

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -1,51 +1,27 @@
 //===============================================================
-const initConnectors = () => {
-  document.querySelectorAll('[data-connector]').forEach((connector) => {
-    let isOpen = false;
-
-    const parent = connector.closest('[data-connect-parent]') || document;
-    const target = parent.querySelector(`[data-connect="${connector.dataset.connector}"]`);
-
-    if (!target) return;
-
-    const toggle = (state) => {
-      isOpen = state;
-      connector.toggleAttribute('data-active', isOpen);
-      target.toggleAttribute('data-active', isOpen);
-    };
-
-    connector.addEventListener('click', (e) => {
-      e.stopPropagation();
-      toggle(!isOpen);
-    });
-
-    target.addEventListener('click', (e) => {
-      e.stopPropagation();
-    });
-
-    document.addEventListener('click', () => {
-      if (isOpen) toggle(false);
-    });
-  });
-};
-
-//===============================================================
 const initMobileMenu = () => {
   let isOpen = false;
 
   const icon = document.querySelector('[data-menu-icon]');
   const menu = document.querySelector('[data-mobile-menu]');
+  const closeBtn = document.querySelector('[data-menu-close]');
 
   const toggleMenu = (state) => {
     isOpen = state;
 
     icon.toggleAttribute('data-active', isOpen);
     menu.toggleAttribute('data-active', isOpen);
+    document.body.classList.toggle('overflow-hidden', isOpen);
   };
 
   icon?.addEventListener('click', (e) => {
     e.stopPropagation();
     toggleMenu(!isOpen);
+  });
+
+  closeBtn?.addEventListener('click', (e) => {
+    e.stopPropagation();
+    toggleMenu(false);
   });
 
   menu?.addEventListener('click', (e) => {
